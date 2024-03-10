b9ac31cb5b3557696dd11fd672e997097d0c12a0
Ayush Nighot <68859871+AyushNighot@users.noreply.github.com>
2024-03-10 11:46:54 +0530

    Update Intersection of two arrays.cpp

--- Intersection of two arrays.cpp
+++ Intersection of two arrays.cpp
@@ -22,3 +22,23 @@ public:
         return ans;
     }
 };
+
+----------------------------------------------------Second Approach---------------------------------------------------------------
+    class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums2.size();
+        vector<int>result;
+        unordered_map<int,int>mp;
+        for(auto &num : nums1){
+            mp[num]++;
+        }
+        for(auto & num : nums2){
+            if(mp.find(num) != mp.end()){
+                result.push_back(num);
+                mp.erase(num);
+            }
+        }
+        return result;
+    }
+};
